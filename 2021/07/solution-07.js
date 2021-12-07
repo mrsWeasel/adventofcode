fcdfb8c53acb67611603c050c1abc0887ece9968
Heino Laura <laura.2.heino@sok.fi>
2021-12-07 11:01:46 +0200

    Day 7, solution 2/2

--- 2021/07/solution-07.js
+++ 2021/07/solution-07.js
@@ -10,24 +10,44 @@ fs.readFile('./data.txt', 'utf-8', (error, data) => {
     const positions = Array(crabs.reduce((a,b) => a >= b ? a : b) + 1).fill(0)
     crabs.map((c)=> positions[c]++)
 
-    let dist = []
+    const dist = []
+    const gas = []
+
+    const adder = (min, max) => {
+        const diff = max - min
+        let sum = 0
+        for (let i = 0; i < diff + 1; i++) {
+            sum += i
+        }
+        return sum
+    }
     
     positions.map((pos, i) => {
         
-        const temp = []
+        const tempDist = []
+        const tempGas = []
         
         for (let j = 0; j < positions.length; j++) {
             // distance to position "i"
             const min = Math.min(i, j)
             const max = Math.max(j, i)
 
-            temp.push((max - min) * positions[j])
+            tempDist.push((max - min) * positions[j])
+            tempGas.push(positions[j] * adder(min, max))
+            
         }
-        dist.push(temp.reduce((a,b)=>a+b))
+        dist.push(tempDist.reduce((a,b)=>a+b))
+        gas.push(tempGas.reduce((a,b)=>a+b))
+       
     })
+
+    // Part 1
+    const smallestDist = dist.reduce((a,b) => a <= b ? a : b)
+    console.log(smallestDist)
     
-    const smallest = dist.reduce((a,b) => a <= b ? a : b)
-    console.log(smallest)
+    // Part 1
+    const smallestGasConsumption = gas.reduce((a,b) => a <= b ? a : b)
+    console.log(smallestGasConsumption)
 })
 
 

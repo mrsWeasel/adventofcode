5e7f0aa20c9bf9c33f88879ef56e17de7915a1f2
Laura Heino <lauramainen@gmail.com>
2022-12-01 08:44:09 +0200

    Day 1/2022

--- 2022/01/index.js
+++ 2022/01/index.js
@@ -1,10 +1,30 @@
-const fs = require("fs");
+const fs = require('fs')
 
-fs.readFile("./data.txt", "utf-8", (error, data) => {
+fs.readFile('./data.txt', 'utf-8', (error, data) => {
   if (error) {
-    console.log(error);
-    return;
+    console.log(error)
+    return
   }
 
-  console.log(data);
-});
+  const total = []
+  const largest = []
+
+  const calories = data
+    .split(/\n{2}/)
+    .map((a) => a.split(/\n/))
+    .map((b) => {
+      const c = b.reduce((a, b) => Number(a) + Number(b), 0)
+      total.push(c)
+    })
+
+  for (let i = 0; i < 3; i++) {
+    largest[i] = total.reduce((a, b) => (a > b ? a : b), 0)
+    total.splice(total.indexOf(largest[i]), 1)
+  }
+
+  // ⭐️ part 1
+  console.log(largest[0])
+
+  // ⭐️⭐️ part 2
+  console.log(largest.reduce((a, b) => a + b, 0))
+})

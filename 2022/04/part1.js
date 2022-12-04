5a4c8f31c0985b72d17bd997370f8c61e81475ff
Laura Heino <lauramainen@gmail.com>
2022-12-04 11:53:54 +0200

    Day 4/2022

--- 2022/04/part1.js
+++ 2022/04/part1.js
@@ -6,5 +6,30 @@ fs.readFile('./data.txt', 'utf-8', (error, data) => {
     return
   }
 
-  console.log(data, 'part1')
+  const isRangeFullyContained = (ranges) => {
+    const first = ranges[0].split('-')
+    const second = ranges[1].split('-')
+
+    if (
+      Number(first[0]) >= Number(second[0]) &&
+      Number(first[1]) <= Number(second[1])
+    )
+      return true
+    if (
+      Number(second[0]) >= Number(first[0]) &&
+      Number(second[1]) <= Number(first[1])
+    )
+      return true
+
+    return false
+  }
+
+  let counter = 0
+
+  data.split(/\n/).map((pair) => {
+    const ranges = pair.split(',')
+    if (isRangeFullyContained(ranges)) counter++
+  })
+
+  console.log(counter)
 })
--- 2022/04/part2.js
+++ 2022/04/part2.js
@@ -6,5 +6,30 @@ fs.readFile('./data.txt', 'utf-8', (error, data) => {
     return
   }
 
-  console.log(data, 'part2')
+  const doRangesOverlap = (ranges) => {
+    const first = ranges[0].split('-')
+    const second = ranges[1].split('-')
+
+    if (
+      Number(first[0]) >= Number(second[0]) &&
+      Number(first[0]) <= Number(second[1])
+    )
+      return true
+    if (
+      Number(second[0]) >= Number(first[0]) &&
+      Number(second[0]) <= Number(first[1])
+    )
+      return true
+
+    return false
+  }
+
+  let counter = 0
+
+  data.split(/\n/).map((pair) => {
+    const ranges = pair.split(',')
+    if (doRangesOverlap(ranges)) counter++
+  })
+
+  console.log(counter)
 })

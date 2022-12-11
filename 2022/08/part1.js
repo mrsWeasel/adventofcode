8e7373e3073f6347055a9b7ebdf12a78b5ff2bb3
Laura Heino <lauramainen@gmail.com>
2022-12-11 14:24:50 +0200

    Day 8/2022

--- 2022/08/part1.js
+++ 2022/08/part1.js
@@ -8,40 +8,57 @@ fs.readFile('./data.txt', 'utf-8', (error, data) => {
 
   // make a 2-dimensional array out of data
   const grid = data.split(/\n/).map((a) => a.split('').map((b) => [Number(b)]))
-  
+
   let counter = 0
 
-  const isVisibleFromLeft = (y, x, height) => {
-    // left edge
-    if (x === 0) return true
-
-    for (let i = 0; i < x; i++) {
+  const isVisibleFromLeft = (y, height, length) => {
+    for (let i = 0; i < length; i++) {
       if (grid[y][i] >= height) return false
     }
+    return true
+  }
 
+  const isVisibleFromTop = (x, height, length) => {
+    for (let i = 0; i < length; i++) {
+      if (grid[i][x] >= height) return false
+    }
+    return true
+  }
+
+  const isVisibleFromRight = (y, x, height, length) => {
+    for (let i = length - 1; i > x; i--) {
+      if (grid[y][i] >= height) return false
+    }
+    return true
+  }
+
+  const isVisibleFromBottom = (y, x, height, length) => {
+    for (let i = length - 1; i > y; i--) {
+      if (grid[i][x] >= height) return false
+    }
     return true
   }
 
   for (let y = 0; y < grid.length; y++) {
     for (let x = 0; x < grid[y].length; x++) {
       const height = grid[y][x]
-      if (isVisibleFromLeft(y, x, height)) {
+
+      if (isVisibleFromLeft(y, height, x)) {
         counter += 1
         continue
       }
-      // if (isVisibleFromRight(tree)) {
-      //   counter += 1
-      //   continue
-      // }  
-      // if (isVisibleFromTop(tree)) {
-      //   counter += 1
-      //   continue
-      // }  
-      // if (isVisibleFromBottom(tree)) {
-      //   counter += 1
-      // }  
+      if (isVisibleFromRight(y, x, height, grid[y].length)) {
+        counter += 1
+        continue
+      }
+      if (isVisibleFromTop(x, height, y)) {
+        counter += 1
+        continue
+      }
+      if (isVisibleFromBottom(y, x, height, grid.length)) {
+        counter += 1
+      }
     }
   }
-
   console.log(counter)
 })
--- 2022/08/part2.js
+++ 2022/08/part2.js
@@ -5,5 +5,74 @@ fs.readFile('./data.txt', 'utf-8', (error, data) => {
     console.log(error)
     return
   }
-    
+
+  // make a 2-dimensional array out of data
+  const grid = data.split(/\n/).map((a) => a.split('').map((b) => [Number(b)]))
+
+  let highest = 0
+
+  const move = (x, y, to, grid) => {
+    try {
+      switch (to) {
+        case 'LEFT':
+          return grid[y][x - 1]
+        case 'RIGHT':
+          return grid[y][x + 1]
+        case 'UP':
+          return grid[y - 1][x]
+        case 'DOWN':
+          return grid[y + 1][x]
+        default:
+      }
+    } catch (e) {
+      return null
+    }
+  }
+
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      let height = grid[y][x]
+
+      const counter = {
+        left: 0,
+        right: 0,
+        up: 0,
+        down: 0,
+      }
+
+      while (true) {
+        const left = move(x - counter.left, y, 'LEFT', grid)
+        if (!left) break
+        counter.left++
+        if (left >= height) break
+      }
+
+      while (true) {
+        const right = move(x + counter.right, y, 'RIGHT', grid)
+        if (!right) break
+        counter.right++
+        if (right >= height) break
+      }
+
+      while (true) {
+        const up = move(x, y - counter.up, 'UP', grid)
+        if (!up) break
+        counter.up++
+        if (up >= height) break
+      }
+
+      while (true) {
+        const down = move(x, y + counter.down, 'DOWN', grid)
+        if (!down) break
+        counter.down++
+        if (down >= height) break
+      }
+
+      const score = counter.left * counter.right * counter.up * counter.down
+
+      if (score > highest) highest = score
+    }
+  }
+
+  console.log(highest)
 })
